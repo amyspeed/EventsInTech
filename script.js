3cea2ff79c088118152893d7b35e0fced5aee12c
Amy Speed <amy.speed.henley@gmail.com>
2018-10-31 18:56:44 -0500

    All files added. HTML inclusive of later JS rendering.

---
+++ script.js
@@ -0,0 +1,15 @@
+//Link to meetup & Eventbrite APIs
+//Handle landing page submit to enter
+//Remove landing page
+//Append home (search) page
+//location: Handle select "other": show hidden zipcode input
+//Date: Handle select "other": show hidden date input
+//Handle submit. Value of zipcode and date used to fetch Meetup and Eventbrite data.
+//Remove home page
+//No results? Append no-results page. 
+    //Handle "Search Again"
+    //Remove No Results page, append cleared home page
+//Results? Append results page.
+    //(All external <a> links should be handled within HTML)
+    //Handle "Search Again"
+    //Remove Results page, append cleared home page
