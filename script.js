78ba21e3b67eb524811342ed8a42e951df08ab2b
Amy Speed <amy.speed.henley@gmail.com>
2018-11-01 03:35:28 -0500

    Added EventBrite API research notes.

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 'use strict';
 //Link to meetup & Eventbrite APIs
+const meetURL='';
+const meetKey='';
+
+const ebURL='https://www.eventbriteapi.com/v3/events/search/?categories=102';
+const ebOAuth='RH6RBMUD3MXBQB2TJWDA';
+//params:
+//sort_by=date
+//location.address=(zipcode)
+//location.within=100mi
+//categories=102 (science and technology)
+//start_date.keyword=this_week
+//Keyword options are "this_week", "next_week", "this_weekend", "next_month", "this_month", "tomorrow", "today"
+//start_date.range_start=
+//token=RH6RBMUD3MXBQB2TJWDA
+//return:
+//events.name.text
+//events.description.text
+//events.vanity_url (for show)
+//events.url (for link)
+//events.start.local
+//events.end.local
 
 //Link to News:
 const newsURL = 'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=9d728f0139044cf3a54a15e546d1851e';
@@ -13,7 +34,6 @@ function fetchNews(responseJson){
 
 function displayNews(responseJson){
   for (let i=0; i<3; i++) {
-    console.log(responseJson.articles[i].title);
     $('.news').append(
         `<h4><a href="${responseJson.articles[i].url}" target="_blank">${responseJson.articles[i].title}</a></h4>
         <img class="news-pic" src="${responseJson.articles[i].urlToImage}">
@@ -32,6 +52,7 @@ function handleEnter() {
     appendHome();
     handleSubmit();
     handleNewSearch();
+    revealDate();
   });
 }
 
@@ -48,27 +69,31 @@ function homePage(){
         </header>
         <form id="main-form">
           <label for="select-location">Select your location</label>
-          <select id="select-location">
+          <select id="select-location" required>
             <option value="">--Choose one--</option>
             <option value="78704">Austin</option>
             <option value="94016">San Francisco</option>
             <option value="10001">New York City</option>
-            <option value="">Other</option>
+            <option value="otherZip">Other</option>
           </select>
 
           <label for="location-other">or input a 5-digit zip code</label>
-          <input id="location-other" type="number" maxlength=5 minlength=5 placeholder="Zip code">
+          <input class="hidden" id="location-other" type="text" pattern="[0-9]" maxlength=5 minlength=5 placeholder="Zip code">
 
           <legend for="select-date">When?</legend>
           <select id="select-date">
-            <option value="">--Choose one--</option>
+            <option value="null">--Choose one--</option>
+            <option value="null">Any Time!</option>
+            <option value="this_month">This Month</option>
+            <option value="next_month">Next Month</option>
+            <option value="this_week">This Week</option>
+            <option value="next_week">Next Week</option>
             <option value="today">Today</option>
-            <option value="Week">This Week</option>
-            <option value="">Other</option>
+            <option value="tomorrow">Tomorrow</option>
+            <option value="enter-date">Enter Date</option>
           </select>  
 
-          <label for="date-other">select a date</label>
-          <input id="date-other" type="date">
+          <input class="hidden" id="date-other" type="date">
           <br>
           <input type="submit" value="Show Me!" id="form-submit">
         </form>  
@@ -77,14 +102,36 @@ function homePage(){
 }
 
 //location: Handle select "other": show hidden zipcode input
+//this function does not work yet:
+function revealZipBox(){
+    let openLocation='otherZip';
+    let selectLocation=$('#select-location').val();
+    $('#select-location').on('click', 'option', function(event){
+    if (openLocation===selectLocation){
+      $('#location-other').removeClass('hidden');
+    }
+  }) 
+}
+
 //Date: Handle select "other": show hidden date input
+//this function does not work yet:
+function revealDate(){
+    let openOption="enter-date";
+    let selectOption=$('#select-date').val();
+    if (openOption === selectOption){
+      console.log('yes!');
+      $('#date-other').removeClass('hidden');
+      //or
+      //$('.main-form').append(`<input id="date-other" type="date">`)
+    }
+}
+
 
 //Handle submit. Value of zipcode and date used to fetch Meetup and Eventbrite data.
-
 function handleSubmit(){
   $('main').on('click', '#form-submit', function (event){
     event.preventDefault;
-    //boolean. Send to Results or No-Results
+    //Send to Results or No-Results
     //appendNoResults();
     appendResults();
   })
@@ -121,14 +168,14 @@ function results(){
         <header>
           <h2>Results</h2>
         </header>
-        <section role="region" id="meetup">
-          <header><h3>Meetup Events</h3></header>
+        <section role="region" id="eventbrite">
+          <header><h3>Eventbrite Events</h3></header>
           <h4><a href="" target="_blank">Example</a></h4>
             <p>Description XXXXX</p>
             <a href="" target="_blank">RSVP Here</a>
             <a href="" target="_blank">Add to Google Calendar</a>
-        <section role="region" id="eventbrite">
-          <header><h3>Eventbrite Events</h3></header>
+        <section role="region" id="meetup">
+          <header><h3>Meetup Events</h3></header>
           <h4><a href="" target="_blank">Example</a></h4>
             <p>Description XXXXX</p>
             <a href="" target="_blank">RSVP Here</a>
